858c688b93284953bfb93a7534356d32fb2358d8
mascarelloa <amfm0793@gmail.com>
2021-09-09 18:42:34 -0400

    styling hero

--- js/script.js
+++ js/script.js
@@ -5,4 +5,18 @@
 //       left : '-'+e.pageX/20,
 //       top : 100+e.pageY/20
 //     })
+//     }
+
+
+// document.addEventListener("mousemove" , parallax);
+// function parallax(e){
+// console. log('111')
+//     this.querySelectorAll('.layer').forEach(layer => {
+//         const speed = layer.getAttribute('data-speed')
+//         const x = (window.innerWidth - e.pageX*speed)/100
+//         const y = (window.innerHeight - e.pageY*speed)/100
+//         layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+// console. log(x)
 //     })
+
+// }
