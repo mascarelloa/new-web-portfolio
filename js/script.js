ce425fa0ad4f6948caba8bd42420a8dcb43b9ef8
mascarelloa <amfm0793@gmail.com>
2021-09-10 00:38:29 -0400

    parallax effect

--- js/script.js
+++ js/script.js
@@ -1,22 +1,19 @@
+(function() {
+    // Add event listener
+    document.addEventListener("mousemove", parallax);
+    const elem = document.querySelector("#parallax");
+    // Magic happens here
+    function parallax(e) {
+        let _w = window.innerWidth/2;
+        let _h = window.innerHeight/2;
+        let _mouseX = e.clientX;
+        let _mouseY = e.clientY;
+        let _depth1 = `${85 - (_mouseX - _w) * 0.01}% ${85 - (_mouseY - _h) * 0.01}%`;
+        let _depth2 = `${85 - (_mouseX - _w) * 0.02}% ${85 - (_mouseY - _h) * 0.02}%`;
+        let _depth3 = `${85 - (_mouseX - _w) * 0.06}% ${85 - (_mouseY - _h) * 0.06}%`;
+        let x = `${_depth3}, ${_depth2}, ${_depth1}`;
+        console.log(x);
+        elem.style.backgroundPosition = x;
+    }
 
-
-// $('html').on('mousemove',function(e){
-//     $('#parallax').offset({
-//       left : '-'+e.pageX/20,
-//       top : 100+e.pageY/20
-//     })
-//     }
-
-
-// document.addEventListener("mousemove" , parallax);
-// function parallax(e){
-// console. log('111')
-//     this.querySelectorAll('.layer').forEach(layer => {
-//         const speed = layer.getAttribute('data-speed')
-//         const x = (window.innerWidth - e.pageX*speed)/100
-//         const y = (window.innerHeight - e.pageY*speed)/100
-//         layer.style.transform = `translateX(${x}px) translateY(${y}px)`
-// console. log(x)
-//     })
-
-// }
+})();
