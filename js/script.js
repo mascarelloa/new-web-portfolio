f4979eaed033e506244d0820f6dd2f8b9302096a
mascarelloa <amfm0793@gmail.com>
2021-12-14 17:03:35 -0500

    navigation and header js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,43 @@
-$(document).ready(function () {
+// $(document).ready(function () {
 
-    $(".menu-btn a").click(function () {
-        $(".overlay").fadeToggle(200);
-        $(this).toggleClass('btn-open').toggleClass('btn-close');
-    });
+//     $(".menu-btn a").click(function () {
+//         $(".overlay").fadeToggle(200);
+//         $(this).toggleClass('btn-open').toggleClass('btn-close');
+//     });
 
-    $('.overlay').on('click', function () {
-        $(".overlay").fadeToggle(200);
-        $(".menu-btn a").toggleClass('btn-open').toggleClass('btn-close');
-    });
+//     $('.overlay').on('click', function () {
+//         $(".overlay").fadeToggle(200);
+//         $(".menu-btn a").toggleClass('btn-open').toggleClass('btn-close');
+//     });
 
-    $('.menu a').on('click', function () {
-        $(".overlay").fadeToggle(200);
-        $(".menu-btn a").toggleClass('btn-open').toggleClass('btn-close');
-    });
+//     $('.menu a').on('click', function () {
+//         $(".overlay").fadeToggle(200);
+//         $(".menu-btn a").toggleClass('btn-open').toggleClass('btn-close');
+//     });
 
-});
+// });
+
+
+window.onscroll = function() {
+    fixedHeader();
+}
+
+var header = document.getElementById("fixed-header");
+var logo = document.getElementById("logo")
+var sticky = header.offsetTop;
+
+
+function fixedHeader(){
+    logo.style.color = "white";
+
+   if (window.pageYOffset > sticky) {
+  header.classList.add("sticky");
+  
+  
+} else {
+    header.classList.remove("sticky");
+   
+}
+
+
+}
